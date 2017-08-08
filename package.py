080093e989ce781501bd13fb833d9737caff33df
tostmohd <30833636+tostmohd@users.noreply.github.com>
2017-08-08 18:32:33 +0530

    Update package.py

--- package.py
+++ package.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2015 ZestyBeanz Technologies Pvt Ltd(<http://www.zbeanztech.com>)
+# 
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http:/'spire_package' module/www.gnu.org/licenses/>.
-#'spire_package' module
+#    T
 ###############################################################################
 from openerp import models, fields, api, _
 import openerp.addons.decimal_precision as dp
